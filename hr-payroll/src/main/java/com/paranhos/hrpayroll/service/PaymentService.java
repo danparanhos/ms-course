dfa24b11699131bd3b323c08848756df90d84ca9
danparanhos <paranhosdan@gmail.com>
2020-11-04 17:25:20 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/paranhos/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/paranhos/hrpayroll/service/PaymentService.java
@@ -1,14 +1,32 @@
 package com.paranhos.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.paranhos.hrpayroll.model.Payment;
+import com.paranhos.hrpayroll.model.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr0worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		 return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId.toString());
+		
+		Worker worker = restTemplate.getForObject(workerId + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 }
--- hr-worker/src/main/java/com/paranhos/hrworker/model/Worker.java
+++ hr-worker/src/main/java/com/paranhos/hrworker/model/Worker.java
@@ -1,5 +1,7 @@
 package com.paranhos.hrworker.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +17,9 @@ import lombok.Setter;
 @Entity
 @Table(name = "tb_worker")
 @Getter @Setter @NoArgsConstructor @EqualsAndHashCode
-public class Worker {
+public class Worker implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
