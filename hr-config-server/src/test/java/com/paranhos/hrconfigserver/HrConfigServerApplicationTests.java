960dbc9db48c93876c40fbf72aa72dcd650ade75
danparanhos <paranhosdan@gmail.com>
2020-11-14 11:15:04 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/paranhos/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paranhos.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
